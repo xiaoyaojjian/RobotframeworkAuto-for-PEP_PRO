3ae4a05ad7ce2731faaff00566e2910c6dddaeb2
xiaoyaojian <q2868765@qq.com>
2016-08-22 19:55:41 +0800

    新增查询用例，合并相关同类项，增加一个get_list_count方法

--- PepExt.py
+++ PepExt.py
@@ -12,3 +12,8 @@ class PepExt:
     def set_pep_value(self, pep,key,value):
         """设置传入的pep对象的键值信息."""
         pep.set_value(key,value)
+		
+    def get_list_Count(self,Heir):
+        """输入框最大输入个数"""
+        m = list(str(Heir)).__len__()
+        return m
